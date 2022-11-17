798b97da3f43e1ff46e2642c9afd60d401551031
Reddlake <reddlake4eva@gmail.com>
2022-11-17 18:16:12 -0400

    added background music

---
+++ index.js
@@ -0,0 +1,8 @@
+let play = document.getElementById('note');
+function playMusic() {
+    let audio = new Audio('TifaTheme.mp3');
+    audio.play()
+}
+play.addEventListener('click', playMusic());
+
+
